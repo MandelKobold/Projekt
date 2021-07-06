762e3613a3a70d6597d71afbe8e8da7292c7d1c9
MandelKobold <rike.cornils@web.de>
2021-07-06 16:02:58 +0200

    File erstellen halb ausgelagert

--- src/CSVOutput.java
+++ src/CSVOutput.java
@@ -14,28 +14,16 @@ public class CSVOutput {
     //Die Daten sinvoll ausgeben
 
     private int rueckgabeWert;
+    private JFileChooser chooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
 
     public void OutputCSV(int[][]ram) throws IOException {
-        int[] gewichtungPlatzierung = gewichtungPlatzierung(ram);
-        System.out.println(Arrays.toString(gewichtungPlatzierung));
-        JFileChooser chooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
 
+        ArrayList<String[]> datalines = createFile(ram);
 
-
-
-        List<String[]> datalines = new ArrayList<>();
-        datalines.add(new String[]{"Rank Acceptability Matrix"}); // Ueberschrift
-        int variante = 1;
-        String[] platzierung = new String[ram[0].length+1];
-        // in der Ersten Zeile die Plaetze aufzaelen
-        platzierung[0] = "";
-        for (int k = 1; k < platzierung.length; k++) {
-            platzierung[k] = "Platz " + k;
-        }
-        datalines.add(platzierung);
         // Dialog zum Speichern von Dateien anzeigen
+        System.out.println("Herbert" + rueckgabeWert);
         rueckgabeWert = chooser.showSaveDialog(null);
-
+        System.out.println(rueckgabeWert);
         if (rueckgabeWert == JFileChooser.APPROVE_OPTION) {
             String path = chooser.getSelectedFile().getAbsolutePath();
             File csvOutputFile;
@@ -47,39 +35,6 @@ public class CSVOutput {
                 csvOutputFile = new File(path + ".csv");
             }
 
-            // Zeilen mit den Ergebnissen in das Ergebnisdokument schreiben
-            for (int[] element : ram) {
-                String[] convert = new String[element.length+1];
-                for (int i = 0; i < element.length; i++) {
-                    convert[0]= "Alternative "+ variante;           // in der ersten Spalte sagen um welche Alternative es sich handelt
-                    convert[i+1] = Integer.toString(element[i]);    // die Zahlen in einen String werfen
-                }
-                variante ++;
-                datalines.add(convert);
-            }
-            //pruefen ob Alternative ausgeschlossen werden kann
-            ArrayList<Integer> ausschluss = new ArrayList<>();
-            ArrayList<Integer> akzeptiert = new ArrayList<>();
-            boolean ausgeschlossen = false;
-            for (int i = 0; i <gewichtungPlatzierung.length ; i++) {
-                //Ausschlusskriterium: heaelfte der Replikationen multipliziert mit dem hoechsten Platz
-                if(gewichtungPlatzierung[i] > 5000*ram.length+1 || gewichtungPlatzierung[i] < 10000) {
-                    ausschluss.add(i+1);
-                    ausgeschlossen = true;
-                }else{
-                    akzeptiert.add(i+1);
-                }
-            }
-
-            //Zeilen an das Ende des Dokuments mit Ergebnissen
-            if(ausgeschlossen && ausschluss.size() != ram.length) {
-                datalines.add(new String[]{""});
-                datalines.add(new String[]{"Die Alternativen " + akzeptiert.toString() + " werden akzeptiert." });
-                datalines.add(new String[]{"Die Alternativen " + ausschluss.toString() + " werden nicht akzeptiert"});
-            }else{
-                datalines.add(new String[]{""});
-                datalines.add(new String[]{"Es konnte keine Alternative ausgeschlossen werden."});
-            }
             try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
                 datalines.stream()
                         .map(this::convertToCSV)
@@ -88,8 +43,55 @@ public class CSVOutput {
                 e.printStackTrace();
             }
 
-
         }
+    }
+
+    private ArrayList<String[]> createFile(int[][]ram){
+        int[] gewichtungPlatzierung = gewichtungPlatzierung(ram);
+        ArrayList<String[]> datalines = new ArrayList<>();
+        datalines.add(new String[]{"Rank Acceptability Matrix"}); // Ueberschrift
+        int variante = 1;
+        String[] platzierung = new String[ram[0].length+1];
+        // in der Ersten Zeile die Plaetze aufzaelen
+        platzierung[0] = "";
+        for (int k = 1; k < platzierung.length; k++) {
+            platzierung[k] = "Platz " + k;
+        }
+        datalines.add(platzierung);
+        // Zeilen mit den Ergebnissen in das Ergebnisdokument schreiben
+        for (int[] element : ram) {
+            String[] convert = new String[element.length+1];
+            for (int i = 0; i < element.length; i++) {
+                convert[0]= "Alternative "+ variante;           // in der ersten Spalte sagen um welche Alternative es sich handelt
+                convert[i+1] = Integer.toString(element[i]);    // die Zahlen in einen String werfen
+            }
+            variante ++;
+            datalines.add(convert);
+        }
+        //pruefen ob Alternative ausgeschlossen werden kann
+        ArrayList<Integer> ausschluss = new ArrayList<>();
+        ArrayList<Integer> akzeptiert = new ArrayList<>();
+        boolean ausgeschlossen = false;
+        for (int i = 0; i <gewichtungPlatzierung.length ; i++) {
+            //Ausschlusskriterium: heaelfte der Replikationen multipliziert mit dem hoechsten Platz
+            if(gewichtungPlatzierung[i] > 5000*ram.length+1 || gewichtungPlatzierung[i] < 10000) {
+                ausschluss.add(i+1);
+                ausgeschlossen = true;
+            }else{
+                akzeptiert.add(i+1);
+            }
+        }
+
+        //Zeilen an das Ende des Dokuments mit Ergebnissen
+        if(ausgeschlossen && ausschluss.size() != ram.length) {
+            datalines.add(new String[]{""});
+            datalines.add(new String[]{"Die Alternativen " + akzeptiert.toString() + " werden akzeptiert." });
+            datalines.add(new String[]{"Die Alternativen " + ausschluss.toString() + " werden nicht akzeptiert"});
+        }else{
+            datalines.add(new String[]{""});
+            datalines.add(new String[]{"Es konnte keine Alternative ausgeschlossen werden."});
+        }
+        return datalines;
     }
     private String convertToCSV(String[] data){
         return Stream.of(data)
